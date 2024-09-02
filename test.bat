990ba9581d8196bb331ee344eba1646f3f843055
waynechen251 <waynechen251@gmail.com>
2024-09-02 14:41:55 +0800

    appsettings

--- test.bat
+++ test.bat
@@ -7,6 +7,9 @@ python .\src\dgregister.py replace_placeholder --config .\sample\Config.json --f
 echo [replace devp-2-1.json]
 python .\src\dgregister.py replace_placeholder --config .\sample\Config.json --file .\sample\DevP\devp-2-1.json --output .\done\devp-2-1.json
 
+echo [replace appsettings.json]
+python .\src\dgregister.py replace_placeholder --config .\sample\Config.json --file .\sample\DevP\appsettings.json --output .\done\appsettings.json
+
 echo [replace smes-7-0-development.json]
 python .\src\dgregister.py replace_placeholder --config .\sample\Config.json --file .\sample\sMES\smes-7-0-development.json --output .\done\smes-7-0-development.json
 
--- testexe.bat
+++ testexe.bat
@@ -7,6 +7,9 @@ echo [replace appsettings-shared.json]
 echo [replace devp-2-1.json]
 .\dgregister.exe replace_placeholder --config .\sample\Config.json --file .\sample\DevP\devp-2-1.json --output .\done\devp-2-1.json
 
+echo [replace appsettings.json]
+.\dgregister.exe replace_placeholder --config .\sample\Config.json --file .\sample\DevP\appsettings.json --output .\done\appsettings.json
+
 echo [replace smes-7-0-development.json]
 .\dgregister.exe replace_placeholder --config .\sample\Config.json --file .\sample\sMES\smes-7-0-development.json --output .\done\smes-7-0-development.json
 
